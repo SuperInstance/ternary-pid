b63c5962cbf4cb3f8dfb9a7b5eb390cc50b49269
SuperInstance <superinstance@users.noreply.github.com>
2026-06-13 12:36:16 -0800

    🔧 Add .gitignore, remove build artifacts from tracking

--- target/debug/deps/ternary_pid-9500bababd1166c9.d
+++
@@ -1,7 +0,0 @@
-/home/phoenix/repos/ternary-pid/target/debug/deps/ternary_pid-9500bababd1166c9.d: src/lib.rs
-
-/home/phoenix/repos/ternary-pid/target/debug/deps/libternary_pid-9500bababd1166c9.rlib: src/lib.rs
-
-/home/phoenix/repos/ternary-pid/target/debug/deps/libternary_pid-9500bababd1166c9.rmeta: src/lib.rs
-
-src/lib.rs:
